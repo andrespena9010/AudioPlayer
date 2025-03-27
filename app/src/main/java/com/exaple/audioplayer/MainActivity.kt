d96a9b7ef87946b6bc3d3b4a88ee9075cd68b89f
Andres Orbys <a.pena.9010@gmail.com>
2025-03-27 12:20:35 +0100

    Configuracion basica inicial.

---
+++ app/src/main/java/com/exaple/audioplayer/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.exaple.audioplayer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.exaple.audioplayer.ui.theme.AudioPlayerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AudioPlayerTheme {
+
+            }
+        }
+    }
+}
